75ead3f402e6da9399c1848bd4334164a3c585cb
Mariah Morales <mjustine622@gmail.com>
2021-02-25 13:50:05 -0500

    commit tree b1e5b93cba6229060b2af52d41fca5a0540bc0dd

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,9 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma(phrase)
+    if speak_to_grandma("I LOVE YOU GRANDMA!")
+        puts "I LOVE YOU TOO PUMPKIN!"
+    end 
+end 
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
